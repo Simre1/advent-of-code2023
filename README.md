2703a2eec65e20cd6a0484f0ec8de0a707b45ae5
Simre1 <simre4775@gmail.com>
2023-12-01 10:13:34 +0100

    Day 1

--- README.md
+++ README.md
@@ -3,23 +3,23 @@
 My solutions to the coding problems in Advent of Code 2022.
 
 - [x] Day 1
-- [x] Day 2
-- [x] Day 3
-- [x] Day 4
-- [x] Day 5
-- [x] Day 6
-- [x] Day 7
-- [x] Day 8
-- [x] Day 9
-- [x] Day 10
-- [x] Day 11
-- [x] Day 12
-- [x] Day 13
-- [x] Day 14
-- [x] Day 15
-- [x] Day 16
-- [x] Day 17
-- [x] Day 18
+- [ ] Day 2
+- [ ] Day 3
+- [ ] Day 4
+- [ ] Day 5
+- [ ] Day 6
+- [ ] Day 7
+- [ ] Day 8
+- [ ] Day 9
+- [ ] Day 10
+- [ ] Day 11
+- [ ] Day 12
+- [ ] Day 13
+- [ ] Day 14
+- [ ] Day 15
+- [ ] Day 16
+- [ ] Day 17
+- [ ] Day 18
 - [ ] Day 19
 - [ ] Day 20
 - [ ] Day 21
--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,23 +1,67 @@
 module Day1.Main where
 
-import qualified Data.Map as M
-import qualified Data.Set as S
-import qualified Data.Vector as V
-import qualified Data.List.Split as LS
-import qualified Data.List as L
-import qualified Data.Text as T
+import Data.Char (isNumber)
+import Data.Functor ((<&>))
+import Data.List qualified as L
+import Data.List.Split qualified as LS
+import Data.Map qualified as M
+import Data.Maybe (fromJust)
+import Data.Set qualified as S
+import Data.Text qualified as T
+import Data.Vector qualified as V
 import Shared
 
-type Input = ()
+type Input = [String]
 
 readInput :: IO Input
-readInput = error "inputs/dayX/input"
+readInput = lines <$> readFile "inputs/day1/example2"
 
 solution1 :: Input -> IO ()
-solution1 input = pure ()
+solution1 input =
+  print $
+    sum $
+      input <&> \line ->
+        let numbers = filter isNumber line
+         in read [head numbers, last numbers]
+
+findOneOf :: [String] -> String -> String
+findOneOf needles str = fromJust $ L.find (`elem` needles) $ L.subsequences str
+
+numbers :: [(String, Char)]
+numbers =
+  [ ("1", '1'),
+    ("2", '2'),
+    ("3", '3'),
+    ("4", '4'),
+    ("5", '5'),
+    ("6", '6'),
+    ("7", '7'),
+    ("8", '8'),
+    ("9", '9'),
+    ("one", '1'),
+    ("two", '2'),
+    ("three", '3'),
+    ("four", '4'),
+    ("five", '5'),
+    ("six", '6'),
+    ("seven", '7'),
+    ("eight", '8'),
+    ("nine", '9')
+  ]
+
+filterLine = filter (`elem` relevantChars)
+  where relevantChars = L.nub $ mconcat $ fst <$> numbers
+
+findFirstNumber line = fromJust (lookup (findOneOf (fst <$> numbers) (filterLine line)) numbers)
+
+findLastNumber line = fromJust (lookup (reverse $ findOneOf (reverse . fst <$> numbers) $ reverse (filterLine line)) numbers)
 
 solution2 :: Input -> IO ()
-solution2 input = pure ()
+solution2 input =
+  print $
+    sum $
+      input <&> \line ->
+        read [findFirstNumber line, findLastNumber line]
 
 main :: IO ()
 main = do
