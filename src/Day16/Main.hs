040aeeb6b61e9c3c85d172e2325d094ccef0b604
Simre1 <simre4775@gmail.com>
2023-12-17 15:26:49 +0100

    Add day 17

--- src/Day16/Main.hs
+++ src/Day16/Main.hs
@@ -1,28 +1,102 @@
 module Day16.Main where
 
 import Control.Applicative.Combinators qualified as P
+import Data.Bifunctor (Bifunctor (..))
 import Data.List qualified as L
 import Data.List.Split qualified as LS
 import Data.Map qualified as M
+import Data.Massiv.Array qualified as A
+import Data.Maybe (maybeToList)
 import Data.Set qualified as S
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Vector qualified as V
+import Debug.Trace (traceShow)
+import Linear (V2 (..))
 import Shared
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as P
 
-type Input = ()
+type Input = LightGrid
+
+newtype LightGrid = LightGrid (A.Array A.U A.Ix2 Char) deriving (Eq, Show, Ord)
 
 readInput :: IO Input
-readInput = error "inputs/dayX/input"
+readInput =
+  parseFileMega "inputs/day16/input" $
+    LightGrid . A.fromLists' A.Seq <$> matrix2DP id
+
+data Direction = DUp | DDown | DLeft | DRight deriving (Eq, Show, Ord)
+
+toIndexDelta :: Direction -> (A.Ix2, Direction)
+toIndexDelta direction = (,direction) $ case direction of
+  DUp -> A.Ix2 (-1) 0
+  DDown -> A.Ix2 1 0
+  DLeft -> A.Ix2 0 (-1)
+  DRight -> A.Ix2 0 1
+
+computeNextDirection :: Direction -> Char -> A.Ix2 -> [(A.Ix2, Direction)]
+computeNextDirection direction char ix = fmap (first (ix +)) $ case char of
+  '.' -> unchangedDirection
+  '-' -> if direction `elem` [DUp, DDown] then splittedDirection else unchangedDirection
+  '|' -> if direction `elem` [DLeft, DRight] then splittedDirection else unchangedDirection
+  '\\' -> case direction of
+    DRight -> [toIndexDelta DDown]
+    DLeft -> [toIndexDelta DUp]
+    DUp -> [toIndexDelta DLeft]
+    DDown -> [toIndexDelta DRight]
+  '/' -> case direction of
+    DRight -> [toIndexDelta DUp]
+    DLeft -> [toIndexDelta DDown]
+    DUp -> [toIndexDelta DRight]
+    DDown -> [toIndexDelta DLeft]
+  _ -> error "Invalid layout"
+  where
+    unchangedDirection = [toIndexDelta direction]
+    splittedDirection =
+      let both = fmap toIndexDelta
+       in case direction of
+            DUp -> both [DRight, DLeft]
+            DDown -> both [DRight, DLeft]
+            DLeft -> both [DUp, DDown]
+            DRight -> both [DUp, DDown]
+
+follow :: LightGrid -> A.Ix2 -> Direction -> S.Set A.Ix2
+follow (LightGrid grid) start startDirection = S.map fst $ go S.empty start startDirection
+  where
+    go visited position direction
+      | (position, direction) `S.member` visited = visited
+      | otherwise =
+          let nextVisited = S.insert (position, direction) visited
+           in case A.index grid position of
+                Nothing -> visited
+                Just gate ->
+                  L.foldl' (\v (nextPosition, nextDirection) -> go v nextPosition nextDirection) nextVisited $
+                    computeNextDirection direction gate position
 
 solution1 :: Input -> IO ()
-solution1 input = pure ()
+solution1 input =
+  print $ length $ follow input (A.Ix2 0 0) DRight
+
+startPositions :: LightGrid -> [(A.Ix2, Direction)]
+startPositions (LightGrid grid) =
+  mconcat
+    [ (\r -> (A.Ix2 r 0, DRight)) <$> [0 .. nR - 1],
+      (\r -> (A.Ix2 r (nC - 1), DLeft)) <$> [0 .. nR - 1],
+      (\c -> (A.Ix2 0 c, DDown)) <$> [0 .. nC - 1],
+      (\c -> (A.Ix2 (nR - 1) c, DUp)) <$> [0 .. nC - 1]
+    ]
+  where
+    (A.Sz2 nR nC) = A.size grid
 
 solution2 :: Input -> IO ()
-solution2 input = pure ()
+solution2 input =
+  let (bestPosition, bestDirection) =
+        head $
+          L.sortOn (negate . length . uncurry (follow input)) $
+            startPositions input
+   in print $ length $ follow input bestPosition bestDirection
 
 main :: IO ()
 main = do
--- src/Day17/Main.hs
+++ src/Day17/Main.hs
@@ -1,9 +1,14 @@
 module Day17.Main where
 
+import Algorithm.Search (aStar)
 import Control.Applicative.Combinators qualified as P
+import Data.Bifunctor (Bifunctor (..))
+import Data.Functor ((<&>))
 import Data.List qualified as L
 import Data.List.Split qualified as LS
 import Data.Map qualified as M
+import Data.Massiv.Array qualified as A
+import Data.Maybe (fromMaybe)
 import Data.Set qualified as S
 import Data.Text (Text)
 import Data.Text qualified as T
@@ -13,16 +18,61 @@ import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as P
 
-type Input = ()
+type Input = City
+
+newtype City = City (A.Array A.U A.Ix2 Int) deriving (Eq, Show, Ord)
+
+cityTopLeft :: City -> A.Ix2
+cityTopLeft (City city) = A.Ix2 0 0
+
+cityBottomRight :: City -> A.Ix2
+cityBottomRight (City city) =
+  let (A.Sz2 r c) = A.size city
+   in A.Ix2 (pred r) (pred c)
 
 readInput :: IO Input
-readInput = error "inputs/dayX/input"
+readInput =
+  parseFileMega "inputs/day17/input" $
+    City . A.fromLists' A.Seq <$> matrix2DP (read . pure)
+
+findBestPath :: (A.Ix2 -> Int -> A.Ix2 -> Bool) -> ((A.Ix2, Int, A.Ix2) -> Int) -> ((A.Ix2, Int, A.Ix2) -> Bool) -> A.Ix2 -> A.Ix2 -> City -> Maybe (Int, [A.Ix2])
+findBestPath turningCondition heuristic isGoal startPosition endPosition (City city) = second (fmap (\(p, _, _) -> p)) <$> aStar nextStates costs heuristic isGoal initial
+  where
+    nextStates (position, straightMoves, lastDirection) =
+      let directions = [A.Ix2 1 0, A.Ix2 (-1) 0, A.Ix2 0 (-1), A.Ix2 0 1]
+          filterNonBackwards = filter (/= negate lastDirection)
+          validDirections = filter (\d -> turningCondition lastDirection straightMoves d || straightMoves == 0) $ filterNonBackwards directions
+       in validDirections <&> \chosenDirection ->
+            ( position + chosenDirection,
+              if abs chosenDirection == abs lastDirection then succ straightMoves else 1,
+              chosenDirection
+            )
+    costs (_, _, _) (nextPosition, _, _) = fromMaybe 999999 $ city A.!? nextPosition
+    initial = (startPosition, 0 :: Int, A.Ix2 0 0)
 
 solution1 :: Input -> IO ()
-solution1 input = pure ()
+solution1 input =
+  let max3Straight lastDirection straightMoves direction = straightMoves < 3 || abs direction /= abs lastDirection
+      endPosition = cityBottomRight input
+      max3StraightHeuristic (position, _, _) =
+        let (A.Ix2 x y) = abs (endPosition - position)
+         in x + y
+   in print (fst <$> findBestPath max3Straight max3StraightHeuristic (\(position, _, _) -> position == endPosition) (cityTopLeft input) (cityBottomRight input) input)
 
 solution2 :: Input -> IO ()
-solution2 input = pure ()
+solution2 input =
+  let min4Max10Straights lastDirection straightMoves direction
+        | straightMoves < 4 = abs lastDirection == abs direction
+        | straightMoves >= 10 = abs lastDirection /= abs direction
+        | otherwise = True
+
+      min4Max10StraightsHeuristic (position, straightMoves, lastDirection) =
+        let predictedPosition = position + lastDirection * max 0 (4 - fromIntegral straightMoves)
+            (A.Ix2 x y) = abs (endPosition - predictedPosition)
+         in x + y
+      endPosition = cityBottomRight input
+      isGoal (position, straightMoves, _) = position == endPosition && straightMoves >= 4
+   in print (fst <$> findBestPath min4Max10Straights min4Max10StraightsHeuristic isGoal (cityTopLeft input) (cityBottomRight input) input)
 
 main :: IO ()
 main = do
